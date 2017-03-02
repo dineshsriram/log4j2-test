ddfc203a56fdc7013c054754ba1bd58bad1e413f
Dinesh Sriram <dsriram1@bloomberg.net>
2017-03-02 11:44:13 -0500

    change log context at runtime

--- src/Log4jExample.java
+++ src/Log4jExample.java
@@ -1,22 +1,37 @@
 package com.dineshsriram.Log4jExample;
 
 import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.core.config.LoggerConfig;
 import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.core.LoggerContext;
+
 import java.io.*;
 
 
 public final class Log4jExample
 {
     static final Logger logger = LogManager.getLogger(Log4jExample.class);
-    
+
     Log4jExample()
     {
-        System.out.println("Starting up!");
-        
+      logger.info("a info msg");
+      logger.warn("a warn msg");
+      logger.debug("a debug msg");
     }
+
     public static void main(String[] args)
     {
         new Log4jExample();
-    }
 
+        LoggerContext ctx = (LoggerContext) LogManager.getContext(false);
+        ctx.getConfiguration().getLoggerConfig(LogManager.ROOT_LOGGER_NAME).setLevel(Level.DEBUG);
+        ctx.updateLoggers(); 
+
+        logger.info("b info msg");
+        logger.warn("b warn msg");
+        logger.debug("b debug msg");
+
+
+    }
 }
